6ba93d206a8233b9f1eef9456a4472e9ae64ae07
Fernanda Xabude <fernandabomfilioli@gmail.com>
2025-11-10 22:32:57 -0300

    Implementando DarkMode e ajustes nas rotas (AppRouter)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,47 @@
 const Header = () => {
+<<<<<<< Updated upstream
   return <div>Header</div>;
+=======
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerSubContainer}>
+        <div className={styles.linkContainer}>
+          <a className={styles.linkHome} href="/">
+            Top
+            <span>Serra</span>
+          </a>
+        </div>
+        <nav className={styles.navContainer}>
+          <ul>
+            <li>
+              <a className={styles.navLinks} href="/horarios">
+                Horários
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/cadastro">
+                Cadastro
+              </a>
+            </li>
+            <li>
+              <a className={styles.navLinks} href="/estoque">
+                Estoque
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.linkContainer}>
+          <a className={styles.linkLogin} href="/login">
+            Login
+          </a>
+          <a className={styles.linkLogin} href="">
+            Logout
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+>>>>>>> Stashed changes
 };
 
 export default Header;
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 const Home = () => {
   return <div>Home</div>;
 };
+=======
+import React from 'react';
+import Header from '../../components/Header/Header';
+>>>>>>> Stashed changes
 
 export default Home;
