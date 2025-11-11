492f9a70c2c7a906a875676c221a0f1db8fc166b
Nathan Lourenço <nathanmlf@hotmail.com>
2025-11-10 21:53:34 -0300

    Correções

--- src/screens/Cadastro/Cadastro.jsx
+++ src/screens/Cadastro/Cadastro.jsx
@@ -1,13 +1,11 @@
 import FormProdutos from "../../components/FormProdutos/FormProdutos.jsx";
 import Header from "../../components/Header/Header.jsx";
-// import Footer from "../../components/Footer/Footer.jsx";
 
 const Cadastro = () => {
   return (
     <>
       <Header />
       <FormProdutos />
-      {/* <Footer />; */}
     </>
   );
 };
